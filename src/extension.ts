9d79266374bdd7129cc8799b1ba3a078cfcbca7e
james <james.bergamin@local.ch>
2020-05-14 08:27:17 +0200

    Rename Elements to TextMatrix

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,10 @@
 import * as vscode from 'vscode';
 
-type Elements = string[][];
+type TextMatrix = string[][];
 
 export function activate(context: vscode.ExtensionContext) {
-	function transpose(elements: Elements): Elements {
-		let result: Elements = [];
+	function transpose(elements: TextMatrix): TextMatrix {
+		let result: TextMatrix = [];
 		for (let c = 0; c < elements[0].length; c++) {
 			const line: string[] = [];
 			for (let i = 0; i < elements.length; i++) {
@@ -15,16 +15,16 @@ export function activate(context: vscode.ExtensionContext) {
 		return result;
 	}
 
-	function flipCharacters(elements: Elements): Elements {
-		let result: Elements = [];
+	function flipCharacters(elements: TextMatrix): TextMatrix {
+		let result: TextMatrix = [];
 		for (let r = 0; r < elements.length; r++) {
 			result.push(elements[r].reverse());
 		}
 		return result;
 	}
 
-	function flipLines(elements: Elements): Elements {
-		let result: Elements = Array.from(elements);
+	function flipLines(elements: TextMatrix): TextMatrix {
+		let result: TextMatrix = Array.from(elements);
 		for (let r = 0; r < result.length / 2; r++) {
 			const temp = result[r];
 			result[r] = result[result.length - r - 1];
@@ -33,7 +33,7 @@ export function activate(context: vscode.ExtensionContext) {
 		return result;
 	}
 
-	function rotate(elements: Elements, degree: number): Elements {
+	function rotate(elements: TextMatrix, degree: number): TextMatrix {
 		switch(degree) {
 			case 90:
 			case -270:
@@ -49,7 +49,7 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	}
 
-	function toString(elements: Elements): string {
+	function toString(elements: TextMatrix): string {
 		let result: string = "";
 		for (let r = 0; r < elements.length; r++) {
 			for (let c = 0; c < elements[r].length; c++) {
@@ -63,8 +63,8 @@ export function activate(context: vscode.ExtensionContext) {
 		return result;
 	}
 
-	function elementsFromDocument(document: vscode.TextDocument): Elements {
-		const elements: Elements = [];
+	function elementsFromDocument(document: vscode.TextDocument): TextMatrix {
+		const elements: TextMatrix = [];
 		let columns = 0;
 		for(let i = 0; i < document.lineCount; i++) {
 			const line = document.lineAt(i);
@@ -82,12 +82,12 @@ export function activate(context: vscode.ExtensionContext) {
 		return elements;
 	}
 
-	function doCommand(command: (elements: Elements) => Elements) {
+	function doCommand(command: (elements: TextMatrix) => TextMatrix) {
 		const editor = vscode.window.activeTextEditor;
 		if (editor) {
 			editor.edit(editBuilder => {
 				try {
-					const elements: Elements = elementsFromDocument(editor.document);
+					const elements: TextMatrix = elementsFromDocument(editor.document);
 					let result = command(elements);
 					
 					editBuilder.replace(new vscode.Range(
